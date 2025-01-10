538d043a92df6c5ad3f4479cbc62f650ebb94f5b
Maxime <maxime.eloir@agenceseize.fr>
2025-01-10 23:40:48 +0100

    webapp3

--- src/app/pwapopup.tsx
+++ src/app/pwapopup.tsx
@@ -2,6 +2,12 @@
 import React, { useState, useEffect } from "react";
 import { subscribeUser, unsubscribeUser, sendNotification } from "./actions";
 
+
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>;
+  userChoice: Promise<{ outcome: 'accepted' | 'dismissed'; platform: string }>;
+}
+
 function urlBase64ToUint8Array(base64String: string) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
