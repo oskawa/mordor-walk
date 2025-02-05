003dae27dbf7c0c4ae4c8a2e2a0ec2b4c0421317
Maxime <maxime.eloir@agenceseize.fr>
2025-02-05 21:39:44 +0100

    authguard

--- src/app/authguard.tsx
+++ src/app/authguard.tsx
@@ -4,17 +4,17 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function AuthGuard({ children }) {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    const userId = localStorage.getItem("userId");
-    const username = localStorage.getItem("username");
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   const userId = localStorage.getItem("userId");
+  //   const username = localStorage.getItem("username");
 
-    if (!token || !userId || !username) {
-      router.push("/"); // Redirect to homepage
-    }
-  }, [router]);
+  //   if (!token || !userId || !username) {
+  //     router.push("/"); // Redirect to homepage
+  //   }
+  // }, [router]);
 
   return <>{children}</>;
 }
