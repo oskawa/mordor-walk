f17463011d82b3ba2f5384aeec1be9a06c60dffc
Maxime <maxime.eloir@agenceseize.fr>
2025-02-05 21:45:47 +0100

    authgard

--- src/app/authguard.tsx
+++ src/app/authguard.tsx
@@ -1,20 +1,29 @@
-'use client';
+"use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function AuthGuard({ children }) {
-  // const router = useRouter();
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // useEffect(() => {
-  //   const token = localStorage.getItem("token");
-  //   const userId = localStorage.getItem("userId");
-  //   const username = localStorage.getItem("username");
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("token");
+      const userId = localStorage.getItem("userId");
+      const username = localStorage.getItem("username");
 
-  //   if (!token || !userId || !username) {
-  //     router.push("/"); // Redirect to homepage
-  //   }
-  // }, [router]);
+      if (!token || !userId || !username) {
+        router.push("/"); // Redirect to homepage
+      } else {
+        setIsAuthenticated(true); // Allow rendering of children
+      }
+    }
+  }, [router]);
+
+  if (!isAuthenticated) {
+    return null; // Prevents flickering before redirecting
+  }
 
   return <>{children}</>;
 }
