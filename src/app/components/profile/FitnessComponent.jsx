cdbd0434b3ec2eabe9aa564043f343e1e5531989
Maxime <maxime.eloir@agenceseize.fr>
2025-07-29 21:32:29 +0200

    disconnect

--- src/app/components/profile/FitnessComponent.jsx
+++ src/app/components/profile/FitnessComponent.jsx
@@ -186,7 +186,7 @@ export default function FitnessComponent() {
     try {
       setIsLoading(true);
       const response = await axios.post(
-        `${NEXT_PUBLIC_WORDPRESS_REST_GLOBAL_ENDPOINT}/userconnection/v1/fitness/disconnect`,
+        `${NEXT_PUBLIC_WORDPRESS_REST_GLOBAL_ENDPOINT}/fitness/v1/disconnect`,
         {
           userId,
           platform,
